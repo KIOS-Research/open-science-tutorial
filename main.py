b6b56c07516dd3414463bc6d08882fc5a5087d27
Marios S. Kyriakou <mariosmsk@gmail.com>
2024-12-04 13:08:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,23 @@ def main(*args):
     parser = argparse.ArgumentParser(description="Process input parameters.")
 
     # Define the expected parameter with a default value
-    parser.add_argument('--inpname', type=int, default=1,
+    parser.add_argument('--inpname', type=str, default='Net1',
                         help="Inpname (default: Net1.inp).")
+    print(11)
 
-    # Manually process '--node_indices=' format if passed as positional args
     processed_args = []
+    print(args)
     for arg in args:
         if arg.startswith('--inpname='):
             key, value = arg.split('=', 1)
             processed_args.extend([key, value])
         else:
-            processed_args.append(arg)
+            print(arg)
+            break
 
-    # Parse the arguments
-    parsed_args = parser.parse_args(processed_args)
-
-    # Retrieve the parsed value
-    inpname = parsed_args.inpname
-    print(inpname)
+    G = epanet(arg + '.inp')
+    h = G.plot()
+    h.savefig('../results/test.png')
 
     # Add all subdirectories of the current working directory to sys.path
     for root, dirs, files in os.walk(os.getcwd()):
@@ -41,11 +40,4 @@ def main(*args):
 if __name__ == "__main__":
     import sys
 
-    main(*sys.argv[1:])
-
-    filename = 'data/Net1.inp'
-    G = epanet(filename)
-    print(G.InputFile)
-    print(G.TempInpFile)
-    h = G.plot()
-    h.savefig('results/test.png')
+    main(*sys.argv[1:])    
